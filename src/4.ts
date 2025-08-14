5787e214b36e4236902edf883a2fb4adec943089
Lorat1403 <lorat1403@gmail.com>
2025-08-14 10:58:55 +0300

    smthing

--- src/4.ts
+++ src/4.ts
@@ -1,4 +1,32 @@
-const key = new Key();
+class Key {
+  private signature: number = Math.random();
+
+  constructor(signature: number) {
+    this.signature = signature;
+  }
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person extends Key {
+  private key: Key;
+
+  getKey() {
+    return this.key;
+  }
+
+}
+
+abstract class House extends Person {
+  public door: boolean;
+ 
+  
+}
+
+
+const key = new Key(25);
 
 const house = new MyHouse(key);
 const person = new Person(key);
